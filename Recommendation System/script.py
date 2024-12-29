46d7e4d08a1231831bd7e1842efa020961d7a1e6
saifeldeeen <saifeldeenm4@gmail.com>
2024-12-29 21:49:36 +0200

    added small description

--- Recommendation System/script.py
+++ Recommendation System/script.py
@@ -1,3 +1,7 @@
+#This is a recommendation System for Movies
+#it will include Action, Sci-Fi, Horror, and a lot more
+#it will also include a function to recommend movies based on user input
+
 from md import movies_data, types
 
 # Global list for filtered movie types
